116dca8f92939218f96634ac126446959564226b
joshrhughes <josh@mtnpine.us>
2018-01-03 18:23:52 -0700

    books post to page now

--- starter-code/app/app.js
+++ starter-code/app/app.js
@@ -6,7 +6,8 @@ $().ready(function() {
   // need to connect API, then parse individual items, post that to the page, connect form fields to accept new data and add to api
   
   //Going to try and connect with the API
-   var books = $.get('https://den-super-crud.herokuapp.com/books')
+   function showBooks(){
+     var books = $.get('https://den-super-crud.herokuapp.com/books')
     .done(function(data){
 
       console.log(data.books[0].title);
@@ -20,12 +21,27 @@ $().ready(function() {
           "<li><span class='info'>Release Date:</span> " + newBooks[i].releaseDate + "</li>" +
           "</ul>");
       }//End for loop
+    
 
+      //Need to post all from info onto page
+      $('form').on("submit", function(event){
+        event.preventDefault();
+        let newBook = {
+          title: $('#book-title').val(),
+          author: $('#book-author').val(),
+          image: $('#book-image').val(),
+          releaseDate: $('#book-release-date').val()
+        };
+        console.log(newBook);
+        //Posting to super crud
+        $.post('https://den-super-crud.herokuapp.com/books', newBook).done(function(data){
+          console.log(data);
+        });
 
-     
+      });
+
+    
     }); // end of .get books
-  
-
-
-
+  }
+  showBooks();
 }); // Ready
